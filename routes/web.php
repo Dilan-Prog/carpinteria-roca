6a15cbce9019c504dbd9f279aed1a1c6d3cf2bb1
MonrroySteph <monrroysteph@gmail.com>
2026-05-19 21:53:04 -0600

    CRUD admin y fron cliente

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\Admin\PedidoController as AdminPedidoController;
+use App\Http\Controllers\Client\PedidoController as ClientPedidoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/prueba', function () {
+    return 'Funciona correctamente';
+});
 
-Route::get('/', function () {
-    return view('welcome');
+// Rutas del Cliente
+Route::get('/consultar', [ClientPedidoController::class, 'showForm'])->name('client.consultar.form');
+Route::get('/consultar/resultado', [ClientPedidoController::class, 'consultar'])->name('client.consultar');
+Route::post('/cancelar', [ClientPedidoController::class, 'cancelar'])->name('client.cancelar');
+
+// Rutas del Admin
+Route::prefix('admin')->group(function () {
+    Route::get('/pedidos', [AdminPedidoController::class, 'index'])->name('pedidos.index');
+    Route::get('/pedidos/create', [AdminPedidoController::class, 'create'])->name('pedidos.create');
+    Route::post('/pedidos', [AdminPedidoController::class, 'store'])->name('pedidos.store');
+    Route::get('/pedidos/{id}/edit', [AdminPedidoController::class, 'edit'])->name('pedidos.edit');
+    Route::put('/pedidos/{id}', [AdminPedidoController::class, 'update'])->name('pedidos.update');
+    Route::patch('/pedidos/{id}/cancelar', [AdminPedidoController::class, 'cancel'])->name('pedidos.cancel');
+    Route::delete('/pedidos/{id}', [AdminPedidoController::class, 'destroy'])->name('pedidos.destroy');
 });
